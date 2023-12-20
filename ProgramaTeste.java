97ef2b8949e6f3b5dbe0810d7b602b3230a35f11
Aislan <99282105+as1lo@users.noreply.github.com>
2023-12-20 18:38:15 -0300

    Add files via upload

---
+++ ProgramaTeste.java
@@ -0,0 +1,12 @@
+package programateste;
+//pacote pode ter várias classes dentro dele, com vários métodos dentro das classes.
+
+public class ProgramaTeste{
+    
+    //HIERARQUIA: método < classe < pacote
+    //método principal que está numa classe PrimeiroPrograma; classe que está dentro do pacote primeiroprograma.
+    
+    public static void main(String[] args){
+        System.out.print("Olá, Mundo!");
+    }
+}
